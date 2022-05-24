d77d40f93fe6487431ee94efe31ddfb76f82d62b
Boukssis_hicham <hichamboukssis@gmail.com>
2022-05-24 17:02:26 +0100

    Create user schema and user validation

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,11 @@ import express from "express";
 import config from "config";
 import connectToDb from "./util/connectToDb";
 import log from "./util/logger";
-
+import router from './routes'
 const app = express();
 const PORT = config.get("port");
+
+app.use(router)
 
 app.listen(PORT, () => {
   log.info(`server is running on ${PORT}`);
